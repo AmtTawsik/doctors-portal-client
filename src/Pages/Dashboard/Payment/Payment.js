2f4522bf04727d40387f766ac299f03f5dad84bd
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-22 09:51:14 +0600

    some edit

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -2,18 +2,17 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData, useNavigation } from 'react-router-dom';
-import Loading from '../../Shared/Loading/Loading';
 import CheckoutForm from './CheckoutForm';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK_key)
 
 const Payment = () => {
     const booking = useLoaderData();
-    const navigation = useNavigation();
+    // const navigation = useNavigation();
     const { treatment, price, appointmentDate, slot } = booking;
-    if(navigation.state === "loading"){
-        return <Loading></Loading>
-    }
+    // if(navigation.state === "loading"){
+    //     return <Loading></Loading>
+    // }
     return (
         <div>
             <h3 className="text-3xl">Payment for {treatment}</h3>
